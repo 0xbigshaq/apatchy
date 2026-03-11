ad764799d5b9ed27795ef30f6f88965b73b55672
0xbigshaq <git@pwner.gg>
2026-03-12 01:46:42 +0200

    JsonOutput: serialize call-tree to json

--- framework/introspector/src/JsonOutput.cpp
+++ framework/introspector/src/JsonOutput.cpp
@@ -1,1 +1,20 @@
-// json output
+
+#include "JsonOutput.h"
+#include "CallTreeNode.h"
+#include <cstdint>
+
+llvm::json::Object JsonOutput::nodeToJson(const struct CallTreeNode &node)
+{
+    llvm::json::Object obj;
+    obj["name"] = node.name;
+    obj["site_file"] = node.site_file;
+    obj["site_line"] = static_cast<int64_t>(node.site_line);
+    obj["site_loc"] = static_cast<int64_t>(node.site_loc);
+
+    llvm::json::Array children;
+    for (const CallTreeNode &child : node.children) {
+        children.push_back(llvm::json::Value(nodeToJson(child)));
+    }
+    obj["children"] = std::move(children);
+    return obj;
+}
--- framework/introspector/src/JsonOutput.h
+++ framework/introspector/src/JsonOutput.h
@@ -1,1 +1,18 @@
-// json output
+#ifndef JSON_OUTPUT_H
+#define JSON_OUTPUT_H
+
+#include "CallTreeNode.h"
+#include <string>
+#include <vector>
+#include <llvm/Support/JSON.h>
+
+class JsonOutput
+{
+  public:
+    JsonOutput()
+    {
+    };
+    llvm::json::Object nodeToJson(const struct CallTreeNode &node);
+};
+
+#endif // JSON_OUTPUT_H
