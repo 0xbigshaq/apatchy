da0311a347246b093507970dfef50d3f604025b3
0xbigshaq <git@pwner.gg>
2026-03-11 21:50:05 +0200

    wuxi: add `FunctionMeta` struct and refactor `FunctionInfo` to return metadata

--- framework/introspector/src/FunctionInfo.h
+++ framework/introspector/src/FunctionInfo.h
@@ -2,13 +2,14 @@
 #include <llvm/Analysis/CallGraph.h>
 #include <llvm/IR/DebugInfoMetadata.h>
 // #include <llvm/IR/Function.h>
+#include "FunctionMeta.h"
 
 using namespace llvm;
 class FunctionInfo
 {
   public:
     FunctionInfo(llvm::Function &func) : m_func(func){};
-    void dump();
+    FunctionMeta dump();
 
   private:
     llvm::Function &m_func;
